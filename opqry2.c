ae75e3cf9a86529943a3700c877d9a691f410a90
gabihirashima <57654394+gabihirashima@users.noreply.github.com>
2020-11-05 00:19:11 -0300

    Update opqry2.c

--- opqry2.c
+++ opqry2.c
@@ -11,3 +11,55 @@
 #include "listaPostos.h"
 #include "listaRegioes.h"
 
+void casosCovid(listaCidade lista, int n, char *cep, char face, int num){
+    Node listaQ = getListaQuadras(lista);
+    Node listaF = getListaFormas(lista);
+    Node quadraCovid;
+    tipo elemento;
+    double x, y, w, h, nw, nh, wx, hy, nx, ny;
+    quadraCovid = comparaCepQuadra(listaQ, cep);
+    x = getXQuadra(quadraCovid);
+    y = getYQuadra(quadraCovid);
+    w = getWQuadra(quadraCovid);
+    h = getHQuadra(quadraCovid);
+
+    switch(face){
+        case 'S':
+            nx = x + (double)num;
+            ny = y + ((5*(y+h))/6);
+            elemento = criaRetangulo(0, (x+w)/6, (y+h)/6, nx, ny, 0, 0, "black", "orange", "1.0px");
+            insertElemento(listaF, elemento);
+            elemento = criaTexto(0, (x+w)/2, (y+h)/2, "black", "white", n);
+            insertElemento(listaF, elemento);
+        break;
+
+        case 'N':
+            nx = x + (double)num;
+            ny = y;
+            elemento = criaRetangulo(0, (x+w)/6, (x+h)/6, nx, ny, 0, 0, "black", "orange", "1.0px");
+            insertElemento(listaF, elemento);
+            elemento = criaTexto(0, (x+w)/2, (y+h)/2, "black", "white", n);
+            insertElemento(listaF, elemento);
+        break;
+
+        case 'L':
+            nx = x + ((5*(x + w))/6);
+            ny = y + (double)num;
+            elemento = criaRetangulo(0, (x+w)/6, (x+h)/6, nx, ny, 0, 0, "black", "orange", "1.0px");
+            insertElemento(listaF, elemento);
+            elemento = criaTexto(0, (x+w)/2, (y+h)/2, "black", "white", n);
+            insertElemento(listaF, elemento);
+
+        break;
+
+        case 'O':
+            nx = x;
+            ny = y + (double)num;
+            elemento = criaRetangulo(0, (x+w)/6, (x+h)/6, nx, ny, 0, 0, "black", "orange", "1.0px");
+            insertElemento(listaF, elemento);
+            elemento = criaTexto(0, (x+w)/2, (y+h)/2, "black", "white", n);
+            insertElemento(listaF, elemento);
+        break;
+    }
+
+}
