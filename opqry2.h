399dc62081ffc59c0f3a529e16f6c53897e390d4
gabihirashima <57654394+gabihirashima@users.noreply.github.com>
2020-11-07 00:55:57 -0300

    Comentários adicionados

--- opqry2.h
+++ opqry2.h
@@ -3,8 +3,12 @@
 
 #include <stdio.h>
 
+/*Coloca um quadrado laranja no endereço num, da quadra identificada por cep e na face determinada. Também insere
+um texto com o número de casos de covid no endereço*/
 void casosCovid(listaCidade lista, int n, char *cep, char face, int num, FILE *saida);
 
+/*Insere um quadrado azul com bordas brancas no endereço num, da quadra identificada por cep e na face determinada.
+Traça uma linha pontilhada até cada um dos n_postos de atendimento. */
 void socorro(listaCidade listacidade, int n_postos, char *cep, char face, int num, FILE *saida);
 
 
